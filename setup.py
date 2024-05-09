d553be80eb94876906022e5ae58d69368388b481
Alexis Morrissey <anm5579@submit02.2e.hpc.psu.edu>
2024-05-09 17:39:35 -0400

    Fixing pypi issues

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
 	name='bio-allo',
-	version='1.1.0',
+	version='1.1.1',
 	author='Alexis Morrissey',
 	author_email='anm5579@psu.edu',
 	packages=['Allo'],
 	python_requires='>=3.10',
 	scripts=['Allo/allo'],
-	url='https://github.com/seqcode/allo/archive/refs/tags/1.1.0.tar.gz',
+	url='https://github.com/seqcode/allo/archive/refs/tags/1.1.1.tar.gz',
 	license='LICENSE.txt',
 	description='A multi-mapped read rescue strategy for ChIP-seq data',
 	include_package_data = True,
